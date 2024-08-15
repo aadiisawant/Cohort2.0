2bd0675445d57f2685a16963e902458451aec533
aadiisawant <adisawant0178@gmail.com>
2024-08-15 22:54:27 +0530

    useEffect and memo

--- Harkirat-Cohort2.0/Week 06/6.1/Wrapper-Component/src/App.jsx
+++ Harkirat-Cohort2.0/Week 06/6.1/Wrapper-Component/src/App.jsx
@@ -1,18 +1,42 @@
 
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
-
-
+  const[todos, setTodos] = useState([])
+  useEffect(()=>{
+    setInterval(()=>{
+      fetch('https://sum-server.100xdevs.com/todos')
+      .then(async (res)=>{
+        const json = await res.json();
+        setTodos(json.todos);
+      })
+    },10000) 
+  },[])
+  async function fetchTodos(){
+    const response = await fetch('https://sum-server.100xdevs.com/todos')
+    const data = await response.json();
+    console.log(data.todos);
+    
+    setTodos(data.todos);
+  }
   return (
     <div>
-      <WrapperComponent InnerComponent={<InnerComp />} />
+      <WrapperComponent InnerComponent={<WrapperComponent InnerComponent={<InnerComp data={todos} fetchTodos={fetchTodos}/>} />} />
+      
     </div>
   )
 }
-function InnerComp(){
+function InnerComp({data, fetchTodos}){
   return <div>
     Hey There!
+    {/* <button onClick={fetchTodos}>Fetch</button> */}
+    {data.map(todo => (
+      <div key={todo.id}>
+        {todo.title}
+        {todo.description}
+      </div>
+    ))}
   </div>
 }
 function WrapperComponent({InnerComponent}){
