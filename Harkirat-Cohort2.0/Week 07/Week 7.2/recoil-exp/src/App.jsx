0fcdca580096e9a0a7b9286bbf1474deb8257591
aadiisawant <adisawant0178@gmail.com>
2024-08-29 00:05:05 +0530

    selector with atom is used

--- Harkirat-Cohort2.0/Week 07/Week 7.2/recoil-exp/src/App.jsx
+++ Harkirat-Cohort2.0/Week 07/Week 7.2/recoil-exp/src/App.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react";
-import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
-import { customCount } from "./Store/atoms/count";
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { customCount, EvenSelector } from "./Store/atoms/count";
 
 export default function App(){
     const [count, setCount] = useState(0);
@@ -9,30 +9,41 @@ export default function App(){
         <div>
           <RecoilRoot>
             <Count />  
+            <EvenRender />
           </RecoilRoot>      
         </div>
     )
 }
 
 function Count(){
+    console.log("re-render");
     
     return <div>
         <RenderCount />
         <Btns />
     </div>
 }
+function EvenRender(){
+  let isEven = useRecoilValue(EvenSelector);
+  return <div>
+    {isEven ? "It is Even" : null}
+  </div>
+}
 
 function RenderCount(){
     let count = useRecoilValue(customCount);
     return <div>
         {count}
+        
     </div>
 }
 
 function Btns(){
-    let [count, setCount]= useRecoilState(customCount)
+    const setCount = useSetRecoilState(customCount)
+    console.log("btns re-render");
+
     return <div>
-        <button onClick={() => setCount(count+1) }>Increment</button>
-        <button onClick={() => setCount(count-1)}>decrement</button>
+        <button onClick={() => setCount(count => count+1) }>Increment</button>
+        <button onClick={() => setCount(count => count-1)}>decrement</button>
     </div>
 }
--- Harkirat-Cohort2.0/Week 07/Week 7.2/recoil-exp/src/Store/atoms/count.jsx
+++ Harkirat-Cohort2.0/Week 07/Week 7.2/recoil-exp/src/Store/atoms/count.jsx
@@ -1,6 +1,15 @@
 
-import {atom} from "recoil"
+import {atom, selector} from "recoil";
+
+
 export const customCount = atom({
-    key : "countAtom",
+    key : "customCount",
     default:0
+})
+export const EvenSelector = selector({
+    key: "EvenSelector",
+    get: ({get}) =>{
+        const count = get(customCount);
+        return count % 2==0 && count!=0;
+    }
 })
